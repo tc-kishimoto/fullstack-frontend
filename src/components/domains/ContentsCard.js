758e7685697fd617920b53200825156544a30e21
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-06-19 10:48:10 +0900

    indexのコンテンツ一覧を追加

---
+++ src/components/domains/ContentsCard.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+const ContentDiv = styled.div`
+    width: 300px;
+    height: auto;
+    background-color: white;
+    border: solid #eee;
+    border-radius: 10px;
+    margin: 10px;
+`
+
+const HeaderDiv = styled.div`
+    display: flex;
+    background-color: white;
+    border-radius: 10px 10px 0 0;
+`
+
+const CardImg = styled.img`
+    margin: 10px;
+    width: 128px;
+    height: 128px;
+`
+
+const H2 = styled.h2`
+    color: #006ef1;
+    margin: auto;
+`
+
+const HeadLink = styled.a`
+    color: #006ef1;
+    position: relative;
+    display: inline-block;
+    text-decoration: none;
+`
+
+const ContentLink = styled.a`
+    color: #006ef1;
+    position: relative;
+    display: inline-block;
+    transition: .3s;
+    text-decoration: none;
+`
+
+const DetailDiv = styled.div`
+    padding: 0 10px 10px 10px;
+    line-height: 1.8;
+    overflow: hidden;
+`
+
+const MoreBtn = styled.a`
+    display: block;
+    background-color: #666;
+    width: 100px;
+    padding: 15px 0;
+    border-radius: 4px;
+    line-height: 12px;
+    text-align: center;
+    font-size: 16px;
+    color: #fff;
+    margin: 10px;
+    cursor: pointer;
+`
+
+function ContentsCard(props) {
+    const categoryName = props.categoryName;
+    const contens =props.contents.map(e => {
+        return (
+            <p>
+                <ContentLink 
+                    href={e}
+                    key={e}>
+                        {e}
+                </ContentLink>
+            </p>
+        );
+    });
+    return(
+        <ContentDiv>
+            <HeaderDiv>
+                <CardImg alt={categoryName} src={`${process.env.PUBLIC_URL}/images/index/${categoryName}.png`} />
+                <H2>
+                    <HeadLink herf="#">{categoryName}</HeadLink>
+                </H2>
+            </HeaderDiv>
+            <DetailDiv>
+                {contens}
+            </DetailDiv>
+            <MoreBtn href="#">open</MoreBtn>
+        </ContentDiv>
+    );
+}
+
+export default ContentsCard;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,6 @@
 import News from "../components/domains/News";
 import CategoryCard from "../components/domains/CategoryCard"
+import ContentsCard from "../components/domains/ContentsCard";
 
 function Index() {
     const categories = [
@@ -24,10 +25,78 @@ function Index() {
         "社会人基礎",
         "開発工程"
     ];
+
+    const contentsList = {
+        "contents": {
+            "DB": [
+                "01_DBMS概要",
+                "02_MySQLの導入",
+                "03_PostgreSQLの導入",
+                "04_SQL入門",
+                "05_SQL応用",
+                "06_DBMSの機能",
+                "07_DB設計",
+                "08_GUIツールの導入",
+                "09_SQL実践編",
+                "10_インデックス",
+                "11_実行計画",
+                "12_NoSQL",
+                "13_MongoDB",
+                "14_MySQLのストアドプロシージャ",
+                "単元課題",
+                "単元課題1",
+                "単元課題2",
+                "演習問題",
+                "演習問題1-模範解答",
+                "演習問題1",
+                "演習問題2-模範解答",
+                "演習問題2",
+                "演習問題3",
+                "演習問題4",
+                "練習問題",
+                "練習問題1-模範解答",
+                "練習問題1",
+                "練習問題2-模範解答",
+                "練習問題2",
+                "練習問題3-模範解答",
+                "練習問題3",
+                "練習問題4-模範解答",
+                "練習問題4",
+                "練習問題5",
+                "練習問題6",
+                "練習問題7",
+                "練習問題8-模範解答",
+                "練習問題8",
+                "練習問題9"
+            ],
+            "IT基礎": [
+                "Excel_00_実習",
+                "Excel_01_ショートカットキー",
+                "Excel_02_基本機能",
+                "Excel_03_関数",
+                "Excel_04_VBA",
+                "Office基礎",
+                "コンピュータ用語辞典",
+                "タイピング&ショートカットキー",
+                "プログラミング入門"
+            ],
+        }
+    }
+
+    const contents = Object.keys(contentsList.contents).map(e => {
+        return (
+            <ContentsCard 
+                categoryName={e} 
+                contents={contentsList.contents[e]} 
+                key={e}
+            />
+        );
+    })
+
     const category = categories.map(e => {
         return <CategoryCard 
-                categoryName={e} 
-                key={e}
+                    categoryName={e} 
+                    key={e}
                 />
     })
     return (
@@ -35,6 +104,8 @@ function Index() {
             <News news={'osirase'}/>
             <p>Fullstack</p>            
             {category}
+            <hr />
+            {contents}
         </div>
     );
 }
