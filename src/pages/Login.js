073b67736f5ee3194f8d61d7a7c96e61a9dcc2b1
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-07-03 23:07:10 +0900

    ログイン処理実装

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -5,6 +7,10 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
 function Login() {
+
+  const [loginId, setLoginId] = useState('');
+  const [password, setPassword] = useState('');
+
     return (
         <Container maxWidth="xs">
           <Box sx={{ bgcolor: 'aliceblue', p: '20px', }} >
@@ -12,17 +18,42 @@ function Login() {
             <TextField 
                 id="loginId" 
                 label="ログインID またはメールアドレス" 
-                variant="filled" />
+                variant="filled" 
+                onChange={e => setLoginId(e.target.value)}
+              />
             <TextField
                 id="password"
                 label="パスワード"
                 variant="filled"
                 type="password"
                 autoComplete="current-password"
-                />
+                onChange={e => setPassword(e.target.value)}
+              />
           </Stack>
             <Stack direction="row" justifyContent="flex-end">
-                <Button margin="normal" variant="contained" sx={{ m: 2 }}>ログイン</Button>
+                <Button margin="normal" variant="contained" 
+                  sx={{ m: 2 }}
+                  onClick={() => {
+                    axios.get(`${process.env.REACT_APP_URL}/sanctum/csrf-cookie`, { withCredentials: true })
+                    .then(res => {
+                      axios.post(`${process.env.REACT_APP_API_URL}/login`, {
+                        email: loginId,
+                        password: password
+                      }, { withCredentials: true })
+                      .then(res => {
+                        if(res.status === 200) {
+                          console.log('ログイン成功')
+                          console.log(res.data)
+                        } else {
+                          console.log('ログイン失敗')
+                        }
+                      }).catch(error => console.log(error))
+                    }).catch(error => console.log(error))
+                  }
+                }
+                >
+                  ログイン
+                </Button>
             </Stack>
           </Box>
         </Container>
