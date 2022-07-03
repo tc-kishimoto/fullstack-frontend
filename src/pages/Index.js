5ebc36f6cca0e8377be7b7f88564abbe9d83f37e
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-07-03 11:54:22 +0900

    トップページにタブを追加

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import News from "../components/domains/News";
 import CategoryCardList from "../components/domains/CategoryCardList";
 import styled from "styled-components";
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import Tab from '@mui/material/Tab';
+import TabPanel from '@mui/lab/TabPanel';
 
 const Container = styled.div`
     margin: 0 auto;
@@ -9,10 +14,23 @@ const Container = styled.div`
 
 function Index() {
 
+    const [tabValue, setTabValue] = useState('1')
+
     return (
         <Container>
             <News news={'osirase'}/>
-            <CategoryCardList></CategoryCardList>
+            <TabContext value={tabValue}>
+                <TabList  aria-label="basic tabs example" onChange={(e, newValue) => setTabValue(newValue)}>
+                    <Tab label="教材一覧" value="1"/>
+                    <Tab label="カテゴリ別" value="2" />
+                </TabList>
+                <TabPanel value="1">
+                    <CategoryCardList></CategoryCardList>
+                </TabPanel>
+                <TabPanel value="2">
+                    comming soon
+                </TabPanel>
+            </TabContext>
         </Container>
     );
 }
