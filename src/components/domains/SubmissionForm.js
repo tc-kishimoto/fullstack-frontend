1b52908d34a8cdc6a78f233fd149164899c4e5c1
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-07-21 22:13:34 +0900

    提出の処理追加

--- src/components/domains/SubmissionForm.js
+++ src/components/domains/SubmissionForm.js
@@ -1,50 +1,78 @@
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import RegisterBtn from './RegistBtn';
+import { useState } from 'react';
 
 function SubmissionForm(props) {
+
+	const dataInit = {
+		category: props.category,
+		lesson_name: props.contentName,
+		url: '',
+		comment: '',
+	}
+	const [data, setData] = useState(dataInit);
+
+	const handleChange = (event) => {
+		const { name, value } = event.target
+		setData(
+			{ ...data, [name]: value }
+		)
+	}
+
 	return (
 		<div>
-			<h2>提出フォーム</h2>
-			<TextField 
-				fullWidth 
-				margin="normal" 
-				id="category" 
-				label="カテゴリ" 
-				variant="outlined" 
-				size="small"
-				defaultValue={props.category}
-				disabled
-			/>
-			<TextField 
-				fullWidth 
-				margin="normal" 
-				id="name" 
-				label="演習名" 
-				variant="outlined" 
-				size="small"
-				defaultValue={props.contentName}
-				disabled
-			/>
-			<TextField 
-				fullWidth 
-				margin="normal" 
-				id="url" 
-				label="提出先URL" 
-				variant="outlined" 
-				size="small" 
-			/>
-			<TextField 
-				fullWidth 
-				margin="normal" 
-				id="comment" 
-				label="コメント" 
-				variant="outlined"
-				multiline
-				rows={4}
-			/>
-			<Stack direction="row" justifyContent="flex-end">
-				<Button margin="normal" variant="contained">送信</Button>
+			<Stack spacing={5}>
+				<h2>提出フォーム</h2>
+				<TextField
+					fullWidth
+					margin="normal"
+					label="カテゴリ"
+					variant="outlined"
+					size="small"
+					name="category"
+					defaultValue={data.category}
+					disabled
+				/>
+				<TextField
+					fullWidth
+					margin="normal"
+					label="演習名"
+					variant="outlined"
+					size="small"
+					name="lesson_name"
+					defaultValue={data.lesson_name}
+					disabled
+				/>
+				<TextField
+					fullWidth
+					margin="normal"
+					label="提出先URL"
+					variant="outlined"
+					size="small"
+					name="url"
+					value={data.url}
+					onChange={handleChange}
+				/>
+				<TextField
+					fullWidth
+					margin="normal"
+					label="コメント"
+					variant="outlined"
+					multiline
+					rows={4}
+					name="comment"
+					value={data.comment}
+					onChange={handleChange}
+				/>
+				<Stack direction="row" justifyContent="flex-end">
+					<RegisterBtn
+						mode={'new'}
+						endpoint={'submissionLesson'}
+						validation={() => true}
+						data={data}
+					/>
+				</Stack>
 			</Stack>
 		</div>
 	);
