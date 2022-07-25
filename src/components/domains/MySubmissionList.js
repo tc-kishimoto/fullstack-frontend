ee8c77c3347c03abba10ee3a1fea3e86ba168a54
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-07-25 22:53:25 +0900

    演習提出フォームを調整

--- src/components/domains/MySubmissionList.js
+++ src/components/domains/MySubmissionList.js
@@ -1,23 +1,10 @@
-import { useEffect, useState } from "react";
-import { useAxios } from '../../service/axios';
 import SubmissionDataGrid from './SubmittionDataGrid';
 
 const MySubmissionList = () => {
 
-  const axios = useAxios();
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchDate = async () => {
-      const result = await axios.get('/getMySubmissions');
-      setData(result.data);
-    };
-    fetchDate();
-  }, [])
-
   return (
     <SubmissionDataGrid 
-      data={data}
+      target={'my'}
     />
   )
 }
--- src/components/domains/SubmittionDataGrid.js
+++ src/components/domains/SubmittionDataGrid.js
@@ -3,22 +3,23 @@ import Button from '@mui/material/Button';
 import { DataGrid } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router-dom';
 import { useAxios } from '../../service/axios';
+import { waitForNone } from "recoil";
 
 const SubmissionDataGrid = (props) => {
 
   const axios = useAxios();
   const navigate = useNavigate();
   const [submissions, setSubmissions] = useState([]);
-
-  const params = props.params;
-
+  
   useEffect(() => {
-    const fetchSubmissions = async() => {
-      const result = await axios.get('/getSubmissions/', params
-      );
-      setSubmissions(result.data);
-    }
-    fetchSubmissions();
+      const params = props.params;
+      const endpoint = props.target === 'my' ? '/getMySubmissions' : '/getSubmissions/'
+      const fetchSubmissions = async() => {
+        const result = await axios.get(endpoint, params
+        );
+        setSubmissions(result.data);
+      }
+      fetchSubmissions();
   }, [])
 
   const columns = [
@@ -26,6 +27,7 @@ const SubmissionDataGrid = (props) => {
       field: 'user_name',
       headerName: 'ユーザー名',
       width: 200,
+      hide: props.target === 'my' ? true : false,
     },
     {
       field: 'category',
