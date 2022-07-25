13b77fa184d70f627d656fafe51f983bf83cc71e
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-07-25 21:39:23 +0900

    無駄削除

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -6,12 +6,10 @@ import { Outlet, useParams } from "react-router-dom";
 
 function MyPage() {
 
-    const { menu } = useParams();
-
     return (
         <Stack direction={'row'} spacing="1" justifyContent="flex-start">
             <UserSideMenu />
-            <Box sx={{ bgcolor: 'aliceblue', p: '20px', width: '100%' }} >
+            <Box sx={{ p: '20px', width: '100%' }} >
                 <Outlet />
             </Box>
         </Stack>
